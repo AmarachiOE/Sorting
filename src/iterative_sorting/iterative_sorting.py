1fa624f8eb2c25437dc0134ba41c9e71d853e761
AmarachiOE <ao.ejiawoko@gmail.com>
2019-07-15 11:24:38 -0700

    First commit

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,5 +1,5 @@
 # TO-DO: Complete the selection_sort() function below 
-def selection_sort( arr ):
+def selection_sort(arr):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
         cur_index = i
