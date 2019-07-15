eea9c6cdecd010833619418e3c716b658c981233
AmarachiOE <ao.ejiawoko@gmail.com>
2019-07-15 15:11:46 -0700

    Bubble_sort test passing. MVP complete.

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -28,8 +28,19 @@ print("Selection Sort: ", result)
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
+    n = len(arr)
+
+    for i in range(0, len(arr) - 1):
+        for j in range(1, len(arr)):
+            if arr[j] < arr[j-1]:
+                arr[j], arr[j-1] = arr[j-1], arr[j] # simple swap
 
     return arr
+
+arr2 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
+print("Original Array: ", arr2)
+result = bubble_sort(arr1)
+print("Selection Sort: ", result)
 
 
 # STRETCH: implement the Count Sort function below
